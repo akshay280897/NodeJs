66cce84cdb62f6668c4771cf5faf99d4588e6987
akshay280897 <akipotdar@gmail.com>
2017-11-25 19:08:02 +0530

    events with setTimeout function

--- hello.js
+++ hello.js
@@ -3,7 +3,11 @@ var http=require("http");
 http.createServer(function (request,response){
 
 	response.writeHead(200,{'Content-type':'text-plain'});
-	response.end('hello world');
+	response.write('Dog is running');
+	setTimeout(function(){
+	response.write('Dog is done');
+	response.end();
+	},5000);
 
 
 }).listen(9999);
